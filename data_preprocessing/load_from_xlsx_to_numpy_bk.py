66ed163235b0592602c6dd61591a0013df456d91
tcl326 <tcl326@nyu.edu>
2018-04-01 23:52:44 -0400

    finish script

--- data_preprocessing/load_from_xlsx_to_numpy_bk.py
+++ data_preprocessing/load_from_xlsx_to_numpy_bk.py
@@ -2,50 +2,50 @@
 # put annotation_format.xlsx in data_collection and this file outside
 
 import openpyxl as px
-import numpy as np 
+import numpy as np
 
 fps = 30
-data_dir = 'data_collection/' 
-xls = px.load_workbook(data_dir + 'annotation_format.xlsx')
-sheet = xls.get_sheet_by_name(name='Sheet1')
+data_dir = '../../gaze-net-data/data_collection/'
+xls = px.load_workbook('annotation_format_new.xlsx')
 
-target_dict = {'r':0, 'g':1, 'b':2, 'y':3, 'o':4, 'p':0}
+target_dict = {'w':0, 'g':1, 'b':2, 'y':3, 'o':4}
 num = 1
 name_list = []
 target_list = []
 start_list = []
 end_list = []
 
-for row in sheet.iter_rows():
-	if sheet.cell(column=5, row=num).value == 'N':
-		num += 1
-		continue
-	name_list.append(sheet.cell(column=1, row=num).value)
-	print(sheet.cell(column=2, row=num).value)
-	target_list.append(target_dict[sheet.cell(column=2, row=num).value])
-	# print(sheet.cell(column=3, row=num).value.minute)
-	# print(sheet.cell(column=4, row=num).value.minute)
-	# print(sheet.cell(column=3, row=num).value)
-	# print(sheet.cell(column=4, row=num).value)
-	# start = sheet.cell(column=3, row=num).value.hour * 60 + sheet.cell(column=3, row=num).value.minute
-	# end = sheet.cell(column=4, row=num).value.hour * 60 + sheet.cell(column=4, row=num).value.minute
-	if type(sheet.cell(column=3, row=num).value) == int:
+set = ['set1', 'set2', 'set3', 'set4']
+for s in set:
+	sheet = xls.get_sheet_by_name(name=s)
+	num = 1
+	for row in sheet.iter_rows():
+		if num == 1:
+			num += 1
+			continue
+		if sheet.cell(column=6, row=num).value == 'N':
+			num += 1
+			continue
+		if sheet.cell(column=1, row=num).value == None:
+			break
+		tmp = sheet.cell(column=1, row=num).value
+		name_list.append(tmp.split('/')[-1])
+		# print(sheet.cell(column=2, row=num).value)
+		target_list.append(target_dict[sheet.cell(column=2, row=num).value])
 		start = sheet.cell(column=3, row=num).value
 		end = sheet.cell(column=4, row=num).value
-	else:
-		start = sheet.cell(column=3, row=num).value.minute * fps
-		end = sheet.cell(column=4, row=num).value.minute * fps + fps
-	start_list.append(start)
-	end_list.append(end)
-	num += 1
+		start_list.append(start)
+		end_list.append(end)
+		num += 1
 
-print(name_list)
-print(target_list)
-print(start_list)
-print(end_list)
+# print(name_list)
+# print(target_list)
+# print(start_list)
+# print(end_list)
+print(len(name_list))
 name_list = np.array(name_list)
 target_list = np.array(target_list)
 start_list = np.array(start_list)
 end_list = np.array(end_list)
-np.savez(data_dir + 'annotation.npz', name_list, target_list, start_list, end_list)
+np.savez('annotation_old.npz', name_list, target_list, start_list, end_list)
 print('save annotation to npy sucessfully!')
--- data_preprocessing/process_annotation_bk.py
+++ data_preprocessing/process_annotation_bk.py
@@ -7,12 +7,11 @@ import os
 import math
 from shutil import copyfile
 
-data_dir = 'data_collection/'
-dst_data_dir = 'gaze_dataset/'
-fps = 30
+data_dir = '../../gaze-net-data/data_collection/'
+dst_data_dir = '../gaze_dataset_old/'
 
 # load npz data
-file = np.load(data_dir + 'annotation.npz')
+file = np.load('annotation_old.npz')
 name_list = file['arr_0']
 target_list = file['arr_1']
 start_list = file['arr_2']
@@ -31,8 +30,8 @@ for i in range(num):
 	print(i)
 	name = name_list[i]
 	target = target_list[i]
-	start_count = start_list[i]
-	end_count = end_list[i]
+	start_count = int(start_list[i])
+	end_count = int(end_list[i])
 	valid_period.append([start_count, end_count])
 
 	directory = os.path.dirname(dst_data_dir + str(target) + '/' + name)
@@ -44,8 +43,13 @@ for i in range(num):
 	if i != 0 and name == name_list[i-1]:
 		add = '_1'
 	img_dst_dir = dst_data_dir + str(target) + '/' + name + add + '/'
+	print(img_dst_dir)
+	if not os.path.exists(img_dst_dir):
+		os.makedirs(img_dst_dir)
 
 	# save valid images
+	print(start_count)
+	print(end_count)
 	for j in range(start_count, end_count):
 		count = str("%06d" % j)
 		img_name = data_dir + name + '/' + name + count + '.jpg'
@@ -60,6 +64,7 @@ for i in range(num):
 
 	# save valid gaze txt
 	gaze_src_path = data_dir + name + '/' + name + 'testfile.txt'
+	# print(gaze_src_path)
 	gaze_dst_path = img_dst_dir + 'gaze.txt'
 	gaze_src_file = open(gaze_src_path)
 	gaze_dst_file = open(gaze_dst_path, 'w')
@@ -81,8 +86,14 @@ for i in range(num):
 			add = ''
 			if s != 0:
 				add = '_1'
-			img_dst_dir = dst_data_dir + str(5) + '/' + name + add + '/'
-			# print(img_dst_dir)
+			img_dst_dir = dst_data_dir + str(5) + '/' + name + add
+			if not os.path.exists(img_dst_dir):
+				# print("get in here")
+				os.makedirs(img_dst_dir)
+			img_dst_dir = img_dst_dir + '/'
+			print(img_dst_dir)
+			print(s)
+			print(e)
 			for j in range(s, e):
 				count = str("%06d" % j)
 				img_name = data_dir + name + '/' + name + count + '.jpg'
@@ -91,10 +102,11 @@ for i in range(num):
 				if not os.path.exists(img_name):
 					continue
 				# no output dir
-				if not os.path.exists(img_dst_dir):
-					os.makedirs(img_dst_dir)
+				# if not os.path.exists(img_dst_dir):
+				# 	print("get in here")
+				# 	os.makedirs(img_dst_dir)
 				copyfile(img_name, img_dst_name)
-			
+
 			# save valid gaze txt
 			gaze_dst_path = img_dst_dir + 'gaze.txt'
 			gaze_dst_file = open(gaze_dst_path, 'w')
@@ -104,7 +116,3 @@ for i in range(num):
 
 		valid_period = []
 		print('clear')
-
-
-
-
