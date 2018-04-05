de431fcb2e82469b7743e515d39ddabd1ca66e67
tcl326 <tcl326@nyu.edu>
2018-04-05 17:16:10 -0400

    original overfitting model added to GazeNetModels

--- GazeNetModels.py
+++ GazeNetModels.py
@@ -1,46 +1,3 @@
-import os
-import sys
-import tensorflow as tf
-import numpy as np
-from keras.models import Model
-from keras.layers import Input, Dense, LSTM, Lambda
-from keras.engine.topology import Input
-from keras import optimizers
-from keras.utils.np_utils import to_categorical
-from keras.models import Sequential, load_model
-from keras.layers import Dropout, Flatten,Concatenate
-from keras.layers import Conv2D, MaxPooling2D, Flatten, Dropout, Conv3D,MaxPooling3D
-from keras.layers import Activation, BatchNormalization, MaxPooling2D
-import time,argparse
-import math
-from keras.utils import plot_model
-from keras.callbacks import ModelCheckpoint
-from keras.preprocessing.image import ImageDataGenerator
-from keras.engine.topology import Input
-# from IPython.display import SVG
-from keras.utils.vis_utils import model_to_dot
-from keras.utils import plot_model
-from keras import backend as K
-K.set_image_dim_ordering('tf')
-import keras.callbacks
-from keras import regularizers
-import gazenetGenerator as gaze_gen
-
-# global param
-dataset_path = '../gaze-net/gaze_dataset'
-learning_rate = 0.0001
-time_steps = 32
-num_classes = 6
-batch_size = 1
-time_skip = 2
-origin_image_size = 360    # size of the origin image before the cropWithGaze
-img_size = 128    # size of the input image for network
-num_channel = 3
-steps_per_epoch=5
-epochs=1
-validation_step=20
-total_num_epoch = 40
-
 
 class GazeNet_3D():
     def __init__(self,learning_rate,time_steps,num_classes,batch_size):
@@ -147,7 +104,7 @@ class GazeNet_3D():
         adam = optimizers.Adam(lr = self.learning_rate)
         model.compile(loss='categorical_crossentropy', optimizer='adam',metrics=['mae', 'acc'])
         print(model.summary())
-        plot_model(model,to_file='model_3D.png',show_shapes=True, show_layer_names=True)
+        # plot_model(model,to_file='model_3D.png',show_shapes=True, show_layer_names=True)
         return model
     def save_model_weights(self,save_path):
 		# Helper function to save your model / weights.
@@ -224,6 +181,7 @@ class GazeNet_2D():
         hidden = Dense(6,kernel_regularizer=regularizers.l2(0.01),
                 activity_regularizer=regularizers.l1(0.01))(hidden)
         output = Lambda(classify)(hidden)
+
         model = Model(input=[image, gaze], output=output)
 
 
@@ -244,7 +202,10 @@ class GazeNet_sequential_2D():
         self.learning_rate = 0.0001
         self.batch_size = batch_size
         self.time_steps = time_steps
+        print("hello world!!!!!!!!!!!!!!!")
+
         self.model = self.create_model()
+
 
     def convolution(self, kernel_size = 3):
         def f(input):
@@ -252,28 +213,29 @@ class GazeNet_sequential_2D():
             conv1 = Conv2D(filters, kernel_size, strides=(3, 3), padding='valid', activation=None)(input)
             conv1 = BatchNormalization()(conv1)
             conv1 = MaxPooling2D(pool_size = 2, padding = 'valid')(conv1)
-            conv1 = Dropout(0.5)(conv1)
+            # conv1 = Dropout(0.5)(conv1)
             conv1 = Conv2D(filters, kernel_size, strides=(2, 2), padding='valid', activation=None)(conv1)
             conv1 = BatchNormalization()(conv1)
             conv1 = MaxPooling2D(pool_size = 2, padding = 'valid')(conv1)
-            conv1 = Dropout(0.5)(conv1)
+            # conv1 = Dropout(0.5)(conv1)
             conv1 = Conv2D(filters, kernel_size, strides=(2, 2), padding='valid', activation=None)(conv1)
             conv1 = BatchNormalization()(conv1)
             conv1 = MaxPooling2D(pool_size = 2, padding = 'valid')(conv1)
-            conv1 = Dropout(0.5)(conv1)
+            # conv1 = Dropout(0.5)(conv1)
             return conv1
         return f
 
     def lstm(self):
         def f(input):
             lstm = LSTM(128,return_sequences=True,stateful=True)(input)
-            lstm = Dropout(0.5)(lstm)
+            # lstm = Dropout(0.5)(lstm)
             lstm = LSTM(128,return_sequences=True,stateful=True)(lstm)
             return lstm
         return f
 
     def create_model(self):
-        image = Input(shape=(256,256,3,))
+        print("hello world!!!!!!!!!!!!!!!")
+        image =Input(shape=(256,256,3,))
         def input_reshape(image):
             return tf.reshape(image,[-1,256,256,3])
         image_reshaped = Lambda(input_reshape)(image)
@@ -306,10 +268,10 @@ class GazeNet_sequential_2D():
         #     return tf.reduce_mean(res,1)
         hidden = Dense(512,kernel_regularizer=regularizers.l2(0.01),
                 activity_regularizer=regularizers.l1(0.01))(merged)
-        hidden = Dropout(0.5)(hidden)
+        # hidden = Dropout(0.5)(hidden)
         hidden = Dense(512,kernel_regularizer=regularizers.l2(0.01),
                 activity_regularizer=regularizers.l1(0.01))(hidden)
-        hidden = Dropout(0.5)(hidden)
+        # hidden = Dropout(0.5)(hidden)
         hidden = Dense(5,kernel_regularizer=regularizers.l2(0.01),
                 activity_regularizer=regularizers.l1(0.01))(hidden)
         # hidden = Lambda(mean_value)(hidden)
@@ -323,9 +285,82 @@ class GazeNet_sequential_2D():
         adam = optimizers.Adam(lr = self.learning_rate)
         model.compile(loss='categorical_crossentropy', optimizer='adam',metrics=['mae', 'acc'])
         print(model.summary())
-        plot_model(model,to_file='model_sequencial_2D.png',show_shapes=True, show_layer_names=True)
+        # plot_model(model,to_file='model_sequencial_2D.png',show_shapes=True, show_layer_names=True)
         return model
     def save_model_weights(self,save_path):
 		# Helper function to save your model / weights.
         self.model.save_weights(save_path)
         # self.model.save(save_path)
+
+class GazeNet_native():
+    def __init__(self,learning_rate,time_steps,num_classes,batch_size):
+        self.learning_rate = 0.0001
+        self.batch_size = batch_size
+        self.time_steps = time_steps
+        self.model = self.create_model()
+
+    def convolution(self, kernel_size = 3):
+        def f(input):
+            filters = 96
+            conv1 = Conv2D(filters, kernel_size, strides=(3, 3), padding='valid', activation=None)(input)
+            conv1 = BatchNormalization()(conv1)
+            conv1 = MaxPooling2D(pool_size = 2, padding = 'valid')(conv1)
+            conv1 = Dropout(0.5)(conv1)
+            conv1 = Conv2D(filters, kernel_size, strides=(2, 2), padding='valid', activation=None)(conv1)
+            conv1 = Dropout(0.5)(conv1)
+            conv1 = Conv2D(filters, kernel_size, strides=(2, 2), padding='valid', activation=None)(conv1)
+            conv1 = MaxPooling2D(pool_size = 2,padding = 'valid')(conv1)
+
+            return conv1
+        return f
+
+    def lstm(self):
+        def f(input):
+            lstm = LSTM(128,return_sequences=True)(input)
+            lstm = Dropout(0.5)(lstm)
+            lstm = LSTM(128)(lstm)
+            return lstm
+        return f
+
+    def create_model(self):
+        image = Input(shape=(self.time_steps,256,256,3,))
+        def input_reshape(image):
+            return tf.reshape(image,[-1,256,256,3])
+        image_reshaped = Lambda(input_reshape)(image)
+        image_embedding = self.convolution()(image_reshaped)
+        image_embedding = Dropout(0.5)(image_embedding)
+        flatten = Flatten()(image_embedding)
+
+        gaze = Input(shape=(self.time_steps,3,))
+        def input_gaze_reshape(input):
+            return tf.reshape(input,[-1,1,3])
+        gaze_reshaped = Lambda(input_gaze_reshape)(gaze)
+        print(gaze_reshaped)
+        gaze_embedding = self.lstm()(gaze_reshaped)
+        print(gaze_embedding)
+
+
+        merged = Concatenate()([flatten, gaze_embedding])
+        hidden = Dense(6)(merged)
+        def classify(input):
+            return tf.nn.softmax(input)
+        def mean_value(input):
+            res = tf.reshape(input,[-1,self.time_steps,num_classes])
+            return tf.reduce_mean(res,1)
+        # hidden = Lambda(mean_value)(hidden)
+        output = Lambda(classify)(hidden)
+
+        model = Model(input=[image, gaze], output=output)
+
+
+        adam = optimizers.Adam(lr = self.learning_rate)
+        model.compile(loss='categorical_crossentropy', optimizer='adam',metrics=['mae', 'acc'])
+        print(model.summary())
+        return model
+    def save_model_weights(self,save_path):
+		# Helper function to save your model / weights.
+
+        self.model.save_weights(save_path)
+        # self.model.save(save_path)
+
+        # return suffix
